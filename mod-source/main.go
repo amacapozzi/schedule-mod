3c09a4d9e02d1155c0a8b1dc3a2528dc3b086731
amacapozzi <amacapozzi07@gmail.com>
2025-03-28 17:33:21 -0300

    Update application window dimensions to 500x400

--- mod-source/main.go
+++ mod-source/main.go
@@ -18,8 +18,8 @@ func main() {
 	// Create application with options
 	err := wails.Run(&options.App{
 		Title:  "mod-source",
-		Width:  1024,
-		Height: 768,
+		Width:  500,
+		Height: 400,
 		AssetServer: &assetserver.Options{
 			Assets: assets,
 		},
